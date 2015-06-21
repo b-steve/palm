a9ed0c36a27135e3dee6942e6d94d879f4c5715f
Ben Stevenson <bcs5@st-andrews.ac.uk>
2015-06-21 22:43:43 +0100

    Getting things working with Rcpp.

--- install-nspp.sh
+++ install-nspp.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 rm -rfv man
 rm -fv NAMESPACE
-#R --slave -e "library(Rcpp); compileAttributes()"
+R --slave -e "library(Rcpp); compileAttributes()"
 R --slave -e "library(roxygen2); roxygenise('.')"
+rm -rfv ..Rcheck/ ..pdf
+rm -rfv src/*.o src/*.so src/*.rds
+rm -rfv src-i386/ src-x64/
 R CMD build .
 mkdir -p package-build
 mv nspp_*.tar.gz package-build/
