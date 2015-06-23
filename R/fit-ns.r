5532947b71375a295e9d390bf23d150a3d910663
Ben Stevenson <bcs5@st-andrews.ac.uk>
2015-06-23 18:00:54 +0100

    Improving optimisation.

--- R/fit-ns.r
+++ R/fit-ns.r
@@ -16,6 +16,9 @@
 #' @param lims A matrix with two columns, corresponding to the upper
 #' and lower limits of each dimension, respectively.
 #' @param R Truncation distance for the difference process.
+#' @param sigma.sv The start value for \code{sigma} in optimisation.
+#' @param sigma.bounds The bounds of the \code{sigma} parameter in
+#' optimisation.
 #' @param child.dist An argument describing the distribution
 #' generating the number of children per parent. It must be a list
 #' with four components: (1) A component named \code{mean} that
@@ -30,10 +33,22 @@
 #' @param non.siblings An object containing information about known
 #' siblings (or known non-siblings). Not yet implemented.
 #' @param sv A vector (or list?) of start values for optimisation.
-fit.ns <- function(points = NULL, lims = NULL, R,
+#'
+#' @export
+fit.ns <- function(points = NULL, lims = NULL, R, sigma.sv = 0.1*R,
+                   sigma.bounds = c(0, R),
                    child.dist = list(mean = function(x) x, var = function(x) x,
                        sv = 5, bounds = c(1e-8, 1e8)),
                    non.siblings = NULL){
+    ## Saving arguments.
+    arg.names <- names(as.list(environment()))
+    args <- vector(mode = "list", length = length(arg.names))
+    names(args) <- arg.names
+    for (i in arg.names){
+        if (!is.null(get(i))){
+            args[[i]] <- get(i)
+        }
+    }
     ## Errors for inconsistent dimensions.
     if (!is.matrix(points)){
         points <- matrix(points, nrow = 1)
@@ -55,32 +70,37 @@ fit.ns <- function(points = NULL, lims = NULL, R,
     }
     ## Calculating survey area.
     area <- prod(apply(lims, 1, diff))
-    ## Sorting out start values.
-    sv <- c(nu.fun(child.dist$sv, child.dist), 0.1*R)
-    names(sv) <- c("nu", "sigma")
-    ## Sorting out bounds.
-    bounds.nu <- nu.fun(child.dist$bounds, child.dist)
-    if (is.nan(bounds.nu[1])) bounds.nu[1] <- 0
-    bounds.sigma <- c(0, Inf)
-    lower <- c(bounds.nu[1], bounds.sigma[1])
-    upper <- c(bounds.nu[2], bounds.sigma[2])
     ## Calculating distances.
     dists <- pbc_distances(points = points, lims = lims)
     ## Truncating distances.
     dists <- dists[dists <= R]
+    n.dists <- length(dists)
+    ## Sorting out start values.
+    nu.sv <- nu.fun(child.dist$sv, child.dist)
+    Dc.sv <- analytic.Dc(nu.sv, sigma.sv, n.dists, n.points, R)
+    sv <- c(Dc.sv, nu.sv, sigma.sv)
+    names(sv) <- c("Dc", "nu", "sigma")
+    ## Sorting out bounds.
+    Dc.bounds <- c(0, Inf)
+    nu.bounds <- nu.fun(child.dist$bounds, child.dist)
+    if (is.nan(nu.bounds[1])) nu.bounds[1] <- 0
+    lower <- c(Dc.bounds[1], nu.bounds[1], sigma.bounds[1])
+    upper <- c(Dc.bounds[2], nu.bounds[2], sigma.bounds[2])
     fit <-  optimx(par = log(sv), fn = ns.nll,
-                   method = "nlminb",
+                   method = "L-BFGS-B",
                    lower = log(lower),
                    upper = log(upper),
                    n.points = n.points, dists = dists, R = R,
-                   n.dims = n.dims, nu.fun = nu.fun)
+                   n.dims = n.dims, nu.fun = nu.fun,
+                   par.names = names(sv))
     ## Extracting sigma and nu estimates.
     opt.pars <- exp(coef(fit)[1, ])
+    names(opt.pars) <- names(sv)
     sigma.par <- opt.pars["sigma"]
     nu.par <- opt.pars["nu"]
     ## Calculating Dc estimate.
-    Dc.par <- (length(dists)/n.points - nu.par +
-                   nu.par*exp((-R^2)/(4*sigma.par^2)))/(pi*R^2)
+    ##Dc.par <- analytic.Dc(nu.par, sigma.par, n.dists, n.points, R)
+    Dc.par <- opt.pars["Dc"]
     ## Calculating parameter of child distribution.
     child.par <- uniroot(function(x, child.dist, nu) nu.fun(x, child.dist) - nu,
                          interval = child.dist$bounds, child.dist = child.dist,
@@ -90,21 +110,26 @@ fit.ns <- function(points = NULL, lims = NULL, R,
     D.par <- Dc.par/mu.par
     pars <- c(D.par, sigma.par, child.par, Dc.par, mu.par, nu.par)
     names(pars) <- c("D", "sigma", "child.par", "Dc", "mu", "nu")
-    pars
+    out <- list(pars = pars, args = args)
+    class(out) <- "nspp"
+    out
 }
 
-ns.nll <- function(pars, n.points, dists, R, n.dims, nu.fun){
+ns.nll <- function(pars, n.points, dists, R, n.dims, nu.fun, par.names){
     ## Extracting parameters.
+    names(pars) <- par.names
     pars <- exp(pars)
+    Dc <- pars["Dc"]
     nu <- pars["nu"]
     sigma <- pars["sigma"]
-    Dc <- (length(dists)/n.points - nu + nu*exp((-R^2)/(4*sigma^2)))/(pi*R^2)
+    ## Can work out Dc analytically.
+    ##Dc <- analytic.Dc(nu, sigma, length(dists), n.points, R)
     vol <- n.points*(pi*Dc*R^2 + nu - nu*exp((-R^2)/(4*sigma^2)))
-    if (nu > (length(dists)/n.points + nu*exp((-R^2)/(4*sigma^2)))){
-        ll <- NA
-    } else {
+    ##if (nu > (length(dists)/n.points + nu*exp((-R^2)/(4*sigma^2)))){
+    ##    ll <- NA
+    ##} else {
         ll <- sum(log(n.points*ns.palm(dists, Dc, nu, sigma, n.dims))) - vol
-    }
+    ##}
     ## Printing parameter values.
     cat("Dc = ", Dc, ", sigma = ", sigma, ", nu = ", nu, ", LL = ", ll, "\n", sep = "")
     -ll
