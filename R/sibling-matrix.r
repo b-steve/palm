71f24b99aa37e6a39f06f7f24af65860e874456f
Ben Stevenson <bcs5@st-andrews.ac.uk>
2015-06-25 16:30:51 +0100

    Adding function to create sibling matrix for two-plane surveys.

---
+++ R/sibling-matrix.r
@@ -0,0 +1,28 @@
+## Functions for creating the sibling matrix.
+
+#' Creating a sibling matrix for two-plane surveys
+#'
+#' Creates an object suitable for the \code{siblings} argument to
+#' \link{fit.ns} for two-plane surveys of whale populations. There is
+#' partial sibling information in that observations made by the same
+#' plane cannot be siblings.
+#'
+#' @return A list, a suitable object for the \code{siglings} argument
+#' to \link{fit.ns}.
+#'
+#' @param plane.id A vector indicating which plane collected each
+#' observed location.
+#'
+#' @export
+twoplane.siblings <- function(plane.id){
+    n.points <- length(plane.id)
+    out <- matrix(NA, nrow = n.points, ncol = n.points)
+    for (i in 1:n.points){
+        for (j in i:n.points){
+            if (plane.id[i] == plane.id[j]){
+                out[i, j] <- FALSE
+            }
+        }
+    }
+    list(matrix = out, pT = 0, pF = 0.5)
+}
