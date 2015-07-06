c43bbb2d3ab327984ea9a94990893f545e2ab2f4
Ben Stevenson <bcs5@st-andrews.ac.uk>
2015-07-06 15:59:28 +0100

    Updating tests.

--- tests/testthat/test-fits.r
+++ tests/testthat/test-fits.r
@@ -6,15 +6,14 @@ test_that(
         fit.pois.1D <- fit.ns(points = example.1D, lims = rbind(c(0, 1)), R = 0.5,
                               child.dist = list(mean = function(x) x,
                                   var = function(x) x,
-                                  sv = 5, bounds = c(0, 1e8)), trace = TRUE)
+                                  sv = 5, bounds = c(0, 1e8)))
         expect_that(abs(coef(fit.pois.1D)[1] - 46.530524823) < 1e-4, is_true())
         fit.bin.1D <- fit.ns(points = example.1D, lims = rbind(c(0, 1)), R = 0.5,
                              child.dist = list(mean = function(x) 4*x,
                                  var = function(x) 4*x*(1 - x),
-                                 sv = 0.5, bounds = c(0, 1)), trace = TRUE)
+                                 sv = 0.5, bounds = c(0, 1)))
         expect_that(abs(coef(fit.bin.1D)[1] - 34.908843190) < 1e-4, is_true())
     })
-
 
 test_that(
     "2D fitting",
@@ -43,9 +42,40 @@ test_that(
         expect_that(abs(plane.data$points[1, 1] - 75.043889) < 1e-4, is_true())
         siblings <- twoplane.siblings(planes)
         ## Fitting model.
-        fit <- fit.ns(points = points, lims = lims, R = 0.5,
+        fit.twoplane <- fit.ns(points = points, lims = lims, R = 0.5,
                       child.dist = list(mean = function(x) 2*0.2/(x + 0.2),
                              var = function(x) 2*x*0.2*(2 - x - 0.2)/(x + 0.2)^2,
                           sv = 0.2, bounds = c(0, 1)), siblings = siblings)
-        expect_that(abs(coef(fit)[1] - 1.387786) < 1e-4, is_true())
+        expect_that(abs(coef(fit.twoplane)[1] - 1.387786) < 1e-4, is_true())
+    })
+
+test_that(
+    "Bootstrapping",
+    {
+        set.seed(5432)
+        fit.pois.1D.boot <- boot.ns(fit = fit.pois.1D,
+                                    rchild.fun = function(n, child.par){
+                                        rpois(n, lambda = child.par)
+                                    },
+                                    N = 5, prog = FALSE)
+        expect_that(abs(mean(fit.pois.1D.boot$boots[, 1]) - 48.787782395) < 1e-4, is_true())
+        rchild.twoplane <- function(n, child.par){
+            p10 <- child.par
+            p01 <- 0.2
+            p11 <- 1 - p10
+            p00 <- 1 - p01
+            p.up <- p01/(p10 + p01)
+            out <- sample(c(0, 1), size = n, replace = TRUE, prob = c(1 - p.up, p.up))
+            for (i in 1:n){
+                if (out[i] == 0){
+                    prob <- c(p00, p01)
+                } else if (out[i] == 1){
+                    prob <- c(p01, p11)
+                }
+                out[i] <- out[i] + sample(c(0, 1), size = 1, replace = TRUE,
+                                          prob = prob)
+            }
+        }
+        expect_that(boot.ns(fit.twoplane, rchild.fun = rchild.twoplane,
+                            N = 10, prog = FALSE), throws_error())
     })
