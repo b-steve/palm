b97a1ab9547f10698c1b0adfa2744def336ca716
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-12-17 00:34:01 +0000

    More work on Thomas process reference class.

--- R/refclass.r
+++ R/refclass.r
@@ -4,13 +4,16 @@
 ## Overall class containing base stuff.
 ######
 base.class <- setRefClass("base", fields = c("points",
-                                                 "lims",
-                                                 "R",
-                                                 "par.names",
-                                                 "par.links",
-                                                 "par.invlinks",
-                                                 "start.pars",
-                                                 "link.start.pars"))
+                                             "n.points",
+                                             "contrasts",
+                                             "lims",
+                                             "dim",
+                                             "R",
+                                             "par.names",
+                                             "par.links",
+                                             "par.invlinks",
+                                             "start.pars",
+                                             "link.start.pars"))
 ## A method for converting functions to their link scale.
 base.class$methods(link.pars = function(pars){
     n.pars <- length(pars)
@@ -18,6 +21,7 @@ base.class$methods(link.pars = function(pars){
     for (i in 1:n.pars){
         out[i] <- par.links[[i]](pars[i])
     }
+    names(out) <- names(pars)
     out
 })
 ## A method for converting functions to their real scale.
@@ -27,29 +31,100 @@ base.class$methods(invlink.pars = function(pars){
     for (i in 1:n.pars){
         out[i] <- par.invlinks[[i]](pars[i])
     }
+    names(out) <- names(pars)
     out
-}
+})
 ## A method for generating link.start.pars.
-base.class$methods(initialise.link.start.pars(){
+base.class$methods(initialise.link.start.pars = function(){
     link.start.pars <<- link.pars(start.pars)
 })
 ## An empty method for the Palm intensity.
-base.class$methods(palm.intensity = function(pars){})
-## An empty method for the Palm likelihood function.
-base.class$methods(palm.likelihood = function(pars){})
+base.class$methods(palm.intensity = function(r, pars){})
+## A default method for the sum of the log Palm intensities.
+base.class$methods(sum.log.intensities = function(pars){
+    sum(log(n.points*palm.intensity(contrasts, pars)))
+})
+## A default method for the integral in the Palm likelihood.
+base.class$methods(palm.likelihood.integral = function(pars){
+    f <- function(r, pars){
+        palm.intensity(r, pars)*Sd(r, dim)
+    }
+    -n.points*integrate(f, lower = 0, upper = R, pars = pars)$value
+})
+## A method for the Palm likelihood function.
+base.class$methods(log.palm.likelihood = function(pars){
+    sum.log.intensities(pars) + palm.likelihood.integral(pars)
+})
 ## A method for the negative Palm likelihood function.
-base.class$methods(neg.palm.likelihood = function(pars){
-    -palm.likelihood(pars)
+base.class$methods(neg.log.palm.likelihood = function(pars){
+    -log.palm.likelihood(pars)
+})
+## A method for the negative Palm likelihood function with linked parameters.
+base.class$methods(link.neg.log.palm.likelihood = function(link.pars){
+    pars <- invlink.pars(link.pars)
+    neg.log.palm.likelihood(pars)
 })
 ## A method for model fitting.
-base.class$methods(fit = function(.self) optim(.self$start.pars, .self$nloglik))
+base.class$methods(fit = function(.self){
+    optim(.self$link.start.pars, .self$link.neg.log.palm.likelihood)
+})
+
+######
+## Class for periodic boundary conditions.
+######
+pbc.class <- setRefClass("pbc", contains = "base")
+## Creating contrasts.
+pbc.class$methods(get.contrasts = function(){
+    contrasts <- pbc_distances(points = points, lims = lims)
+    contrasts <<- contrasts[contrasts <= R]
+})
 
 ######
 ## Class for Neyman-Scott processes.
 ######
-ns.class <- setRefClass("ns", fields = "child.dist", contains = "base")
+ns.class <- setRefClass("ns", contains = "base")
 ## Overwriting method for the Palm intensity.
-ns.class$methods(palm.intensity = function(pars) sibling.pi(pars) + nonsibling.pi(pars))
+ns.class$methods(palm.intensity = function(r, pars) sibling.pi(r, pars) + nonsibling.pi(pars))
+## An empty method for the expectation of the child distribution (filled by fit.ns()).
+ns.class$methods(child.expectation = function(pars){})
+## An empty method for the variance of the child distribution (filled by fit.ns()).
+ns.class$methods(child.variance = function(pars){})
+## A method for the expected number of siblings from a randomly chosen point.
+ns.class$methods(sibling.expectation = function(pars){
+    (child.variance(pars) + child.expectation(pars)^2)/child.expectation(pars) - 1
+})
+## An empty method for the PDF of Q, the between-sibling distances.
+ns.class$methods(fq = function(r, pars){})
+## An empty method for the CDF of Q.
+ns.class$methods(Fq = function(r, pars){})
+## A default method for the quotient of the PDF of Q and the surface volume.
+ns.class$methods(q.over.s = function(r, pars) fq(r, pars)/Sd(r, dim))
+## A method for the PI of nonsibling points.
+ns.class$methods(nonsibling.pi = function(pars) pars["D"]*child.expectation(pars))
+## A method for the PI of sibling points.
+ns.class$methods(sibling.pi = function(r, pars) sibling.expectation(pars)*q.over.s(r, pars))
+
+######
+## Class for Thomas processes.
+######
+thomas.class <- setRefClass("thomas", contains = "ns")
+## Overwriting method for the PDF of Q.
+thomas.class$methods(fq = function(r, pars){
+    2^(1 - dim/2)*r^(dim - 1)*exp(-r^2/(4*pars["sigma"]^2))/((pars["sigma"]*sqrt(2))^dim*gamma(dim/2))
+})
+## Overwriting method for the CDF of Q.
+thomas.class$methods(Fq = function(r, pars){
+    pgamma(r^2/(4*pars["sigma"]^2), dim/2)
+})
+## Overwriting method for the quotient of the PDF of Q and the surface volume.
+thomas.class$methods(q.over.s = function(r, pars){
+    exp(-r^2/(4*pars["sigma"]^2))/((2*pars["sigma"])^dim*pi^(dim/2))
+})
+
+######
+## Class for Matern processes.
+######
+matern.class <- setRefClass("matern", contains = "ns")
 
 ######
 ## Class for sibling models.
@@ -58,15 +133,5 @@ sibling.class <- setRefClass("sibling", fields = c("sibling.mat",
                                                    "sibling.pT",
                                                    "sibling.fF"),
                              contains = "ns")
-
-######
-## Class for Thomas processes.
-######
-thomas.class <- setRefClass("thomas", contains = "ns")
-
-######
-## Class for Matern processes.
-######
-matern.class <- setRefClass("matern", contains = "ns")
 
 
