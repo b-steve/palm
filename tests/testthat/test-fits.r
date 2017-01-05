a95858e8557af88f11c2b684254456bdc51570b8
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2017-01-05 17:38:52 +1300

    Adding test for refclass fits.

--- tests/testthat/test-fits.r
+++ tests/testthat/test-fits.r
@@ -9,6 +9,10 @@ test_that(
                                                 var = function(x) x,
                                                 sv = 5, bounds = c(0, 1e8)))
         expect_that(abs(coef(fit.pois.1D)[1] - 46.530524823) < 1e-4, is_true())
+        fit.pois.1D.refclass <- fit.ns_refclass(example.1D, lims = rbind(c(0, 1)), R = 0.5)
+        diff.ratio.1D <- fit.pois.1D.refclass[c(1, 3, 2)]/coef(fit.pois.1D)
+        names(diff.ratio.1D) <- NULL
+        expect_equal(diff.ratio.1D, expected = rep(1, 3), tolerance = 0.01)
         fit.bin.1D <- fit.ns(points = example.1D, lims = rbind(c(0, 1)), R = 0.5,
                              child.dist = list(mean = function(x) 4*x,
                                  var = function(x) 4*x*(1 - x),
@@ -40,6 +44,11 @@ test_that(
                               child.dist = list(mean = function(x) x, var = function(x) x,
                                   sv = 20, bounds = c(1e-6, nrow(example.2D))))
         expect_that(abs(coef(fit.pois.2D)[1] - 41.10442073) < 1e-4, is_true())
+        fit.pois.2D.refclass <- fit.ns_refclass(example.2D, lims = rbind(c(0, 1), c(0, 1)),
+                                                R = 0.5)
+        diff.ratio.2D <- fit.pois.2D.refclass[c(1, 3, 2)]/coef(fit.pois.2D)
+        names(diff.ratio.2D) <- NULL
+        expect_equal(diff.ratio.2D, expected = rep(1, 3), tolerance = 0.01)
         fit.bin.2D <- fit.ns(points = example.2D, lims = rbind(c(0, 1), c(0, 1)), R = 0.5,
                              child.dist = list(mean = function(x) 2*x,
                                  var = function(x) 2*x*(1 - x),
